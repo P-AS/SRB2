54b6a1dfd9a0cdf89a5760f59c5c9128f0be300c
PAS <pasrc@proton.me>
2026-09-15 07:28:00 -0400

    Support HIDPI mode on iOS
    iPad windowing still not working

--- src/sdl/i_video.c
+++ src/sdl/i_video.c
@@ -1679,6 +1679,11 @@ static SDL_bool Impl_CreateWindow(SDL_bool fullscreen)
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 #endif
 
+#ifdef IOS
+	// iOS requires apps to size their content based on screen coordinates rather than content size.
+	flags |= SDL_WINDOW_ALLOW_HIGHDPI;
+#endif
+
 	// Create a window
 	window = SDL_CreateWindow("SRB2 "VERSIONSTRING, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 			realwidth, realheight, flags);
